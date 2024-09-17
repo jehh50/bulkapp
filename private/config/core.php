6c7e2443ff6728ba96b1fc0a9768c85060e7b4fc
Javier Hernández <jhernandez50@gmail.com>
2024-09-17 00:44:07 -0400

    Add database cpanel credentials

--- private/config/core.php
+++ private/config/core.php
@@ -9,13 +9,13 @@ define('DB_PASS','');
 define('DB_NAME','bulk2');
 define('DB_PORT','3306');
 
-#Constantes de conexión a BDD ISSABEL
-// define('DB_AHOST','192.168.5.100');
-// define('DB_AUSER','bulksales');
-// define('DB_APASS','123456');
-// define('DB_ANAME','call_center');
-// define('DB_APORT','3306');
-
+#cPanel Credentials
+#Constantes de conexion a BDD
+// define('DB_HOST','localhost');
+// define('DB_USER','ddremwdt_pringles');
+// define('DB_PASS','g%P2Ckvm&lT&');
+// define('DB_NAME','ddremwdt_crm');
+// define('DB_PORT','3306');
 
 #Constantes de la APP
 define('HTML_DIR','private/views/');
